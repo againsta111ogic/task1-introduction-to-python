af1a4679b9b3c58e27fadd5a0d7c33ca0a3385ff
Dmitry Ivanov <dmitry.ivanov08202003@gmail.com>
2025-03-05 13:55:21 +0300

    Fixed access to db and some minor fixes

--- db/db_connector.py
+++ db/db_connector.py
@@ -1,9 +1,13 @@
 import mysql.connector
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def connect_to_db():
     return mysql.connector.connect(
-    host='localhost',
-    database = 'my_database',
-    user='root',
-    password='secretpswrd1'
+    host=os.getenv('DB_HOST'),
+    database = os.getenv('DB_NAME'),
+    user=os.getenv('DB_USER'),
+    password=os.getenv('DB_PASSWORD')
     )
--- db/queries.sql
+++ db/queries.sql
@@ -1,10 +1,10 @@
--- №1 Список комнат и количество студентов в каждой из них
+-- №1 Rooms list and amount of students in each of them.
 SELECT rooms.id, COUNT(students.id) as num_students
 FROM rooms
 LEFT JOIN students
 ON rooms.id = students.room
 GROUP BY rooms.id, rooms.name;
--- №2 5 комнат, где самый маленький средний возраст студентов
+-- №2 5 rooms with the smallest average age of students.
 SELECT rooms.id, CAST(AVG(TIMESTAMPDIFF(YEAR, students.birthday, CURDATE())) AS FLOAT) AS avg_age
 FROM rooms
 INNER JOIN students
@@ -12,7 +12,7 @@ ON rooms.id = students.room
 GROUP BY rooms.id, rooms.name
 ORDER BY avg_age ASC
 LIMIT 5;
--- №3 5 комнат с самой большой разницей в возрасте студентов
+-- №3 5 rooms with the biggesst difference in students age.
 SELECT rooms.id, 
 MAX(TIMESTAMPDIFF(YEAR, students.birthday, CURDATE())) - MIN(TIMESTAMPDIFF(YEAR, students.birthday, CURDATE())) AS age_diff
 FROM rooms
@@ -21,7 +21,7 @@ ON rooms.id = students.room
 GROUP BY rooms.id, rooms.name
 ORDER BY age_diff DESC
 LIMIT 5;
--- №4 Список комнат где живут разнополые студенты
+-- №4 List of rooms where students of different sexes live
 SELECT rooms.id 
 FROM rooms
 INNER JOIN students
